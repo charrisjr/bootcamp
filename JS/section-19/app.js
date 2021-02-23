4d48ea560799122274c8f76c5168b8da18134397
charrisjr <charrisjr@gmail.com>
2021-02-22 20:26:20 -0800

    Section 19 - through exercise 33

---
+++ JS/section-19/app.js
@@ -0,0 +1,10 @@
+// for (i=1; i<=10; i++){
+//     console.log(i)
+// }
+// console.log('DONE !')
+
+const animals = ['dog', 'cat', 'bird', 'fish']
+
+for (let i=0; i < animals.length; i+=1) {
+    console.log (i, animals[i])
+}
