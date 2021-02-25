069778598e06cde033ffd2adde2a3b7346954578
charrisjr <charrisjr@gmail.com>
2021-02-25 11:18:20 -0800

    Complete section 19

--- JS/section-19/app.js
+++ JS/section-19/app.js
@@ -3,8 +3,84 @@
 // }
 // console.log('DONE !')
 
+// const animals = ['dog', 'cat', 'bird', 'fish']
+
+// for (let i=0; i < animals.length; i+=1) {
+//     console.log (i, animals[i])
+// }
+
+// for (let i=1; i <=10; i++) {
+//     console.log (`i is: ${i}`)
+//     for (let j=1; j<=5; j++) {
+//         console.log(`...j is: ${j}`)
+//     }
+// }
+
+// const seatingChart = [
+//     ['Kristen', 'Bob', 'Namita'],
+//     ['Jeff', 'Juanita', 'DeAndre'],
+//     ['Yuma', 'Isaac', 'Nellie', 'Chauncy']
+// ]
+
+// for (let i=0; i < seatingChart.length; i++){
+//     const row = seatingChart[i];
+//     console.log (`students in row ${i+1}: `)
+//     for (let j=0; j < row.length; j++){
+//         console.log(row[j])
+//     }
+// }
+
+// let count = 0;
+// while (count <10) {
+//     console.log(count)
+//     count++;``
+// }
+
+// const SECRET = "PorkChops";
+// let guess = prompt("enter the secret code: ");
+// while (guess !== SECRET) {
+//     guess = prompt("enter the secret code: ");
+// }
+// console.log('You guessed it!')
+
+// let input = prompt('say something');
+// while (true) {
+//     input = prompt(input);
+//     if (input.toLowerCase() === 'stop it')
+//     break;
+// }
+// console.log('OK')
+
+//GUESSING GAME
+
+// let maxNum = parseInt(prompt('what is maximum number? '));
+// while (!maxNum) {
+//     maxNum = parseInt(prompt('enter a valid number: '));
+// }
+// const secretNum = Math.floor((Math.random() * maxNum)) + 1;
+// console.log(secretNum)
+
+// let guess = parseInt(prompt('enter guess: '));
+// let attempts = 1
+// while (guess !== secretNum) {
+//     attempts++
+//     if (guess > secretNum) {
+//         guess = parseInt(prompt('too high.  try again: '));
+//     }
+//     else if (guess < secretNum) {
+//         guess = parseInt(prompt('too low.  try again: '));
+//     } 
+// }
+// console.log(`You guessed it:  ${secretNum}.
+// It took you ${attempts} tries.`)
+
+
+//FOR...OF LOOPS
 const animals = ['dog', 'cat', 'bird', 'fish']
 
-for (let i=0; i < animals.length; i+=1) {
-    console.log (i, animals[i])
+// for (let i=0; i < animals.length; i++) {
+//     console.log(`hi, little ${animals[i]}`)
+// }
+for (let animal of animals) {
+    console.log(`hi, little ${animal}`) // does not require index when iterate over arrays
 }
---
+++ JS/section-19/todo.js
@@ -0,0 +1,27 @@
+let command = prompt('enter command [ add | list | delete | quit ]: ');
+const todoList =['get apples', 'call Mom'];
+while (command !== 'quit' && command != 'q') {
+    if (command === 'list') {
+        console.log ('Here is your todo list:')     // display todo list items
+        for (let i=0; i < todoList.length; i++) { 
+            console.log(`${i}: ${todoList[i]}`);
+        }
+        console.log ('### end of list ###:')
+    }
+    else if (command === 'add') {
+        const newItem = prompt('enter item to ADD: ');
+        todoList.push(newItem);                     // add item to todoList array
+        console.log(`${newItem} added to list`);    // display item addded
+    }
+    else if (command === 'delete') {
+        console.log ('Here is your todo list:')     // display todo list items
+        for (let i=0; i < todoList.length; i++) { 
+            console.log(`${i}: ${todoList[i]}`);
+        }                                           
+        console.log ('### end of list ###:')
+        const itemToDelete = parseInt(prompt('enter item number you wish to DELETE: '));  // get input from user
+        console.log(`${todoList[itemToDelete]} will be removed from list`); // display item to be deleted
+        todoList.splice(itemToDelete,1);                                    // remove item from todoList array
+    }
+    command = prompt('enter command [ add | list | delete | quit ]: ');
+}
